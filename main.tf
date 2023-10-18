09f122147507d340e617c1dcb5c41f767e8b74a7
Yago Alexandre <goltarayago@gmail.com>
2023-10-17 21:43:08 -0300

    add more droplets

--- main.tf
+++ main.tf
@@ -14,11 +14,37 @@ provider "digitalocean" {
 
 resource "digitalocean_droplet" "vm_rancher" {
   image    = var.droplet_image
-  name     = var.droplet_name
+  name     = var.droplet_name_rancher
   region   = var.region
   size     = var.droplet_setup
   ssh_keys = [data.digitalocean_ssh_key.ssh_fedora.id]
 }
+
+resource "digitalocean_droplet" "vm_k8s_1" {
+  image    = var.droplet_image
+  name     = var.droplet_name_k8s_1
+  region   = var.region
+  size     = var.droplet_setup
+  ssh_keys = [data.digitalocean_ssh_key.ssh_fedora.id]
+}
+
+resource "digitalocean_droplet" "vm_k8s_2" {
+  image    = var.droplet_image
+  name     = var.droplet_name_k8s_2
+  region   = var.region
+  size     = var.droplet_setup
+  ssh_keys = [data.digitalocean_ssh_key.ssh_fedora.id]
+}
+
+resource "digitalocean_droplet" "vm_k8s_3" {
+  image    = var.droplet_image
+  name     = var.droplet_name_k8s_3
+  region   = var.region
+  size     = var.droplet_setup
+  ssh_keys = [data.digitalocean_ssh_key.ssh_fedora.id]
+}
+
+
 
 data "digitalocean_ssh_key" "ssh_fedora" {
   name = var.ssh_key_name
@@ -26,7 +52,10 @@ data "digitalocean_ssh_key" "ssh_fedora" {
 
 variable "token" {}
 variable "region" {}
-variable "droplet_name" {}
+variable "droplet_name_rancher" {}
+variable "droplet_name_k8s_1" {}
+variable "droplet_name_k8s_2" {}
+variable "droplet_name_k8s_3" {}
 variable "droplet_setup" {}
 variable "droplet_image" {}
 variable "ssh_key_name" {}
